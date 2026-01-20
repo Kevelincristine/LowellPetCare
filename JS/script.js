b7b934bbf7155ff001bb280c5b553d63dc1b3e40
Kevelin Cristine Santos <kevelincristine82@gmail.com>
2026-01-20 08:56:14 -0300

    Add about section with image slider to homepage
    Introduced a new 'About Us' section in index.html with descriptive text and an image slider showcasing clinic images. Added corresponding CSS styles for the about section and slider, included three new images, and implemented slider functionality in script.js.

--- JS/script.js
+++ JS/script.js
@@ -7,3 +7,31 @@ menu.addEventListener("click",()=>{
     navmenu.classList.toggle("active");
     icon.classList.toggle("open");
 })
+
+const imagens =[
+    "../IMG/img-aboutOne.png",
+    "../IMG/img-aboutTwo.png",
+    "../IMG/img-aboutThree.png"
+];
+let currentIndex = 0;
+
+const imgElement = document.querySelector(".slider-image");
+const LeftArrow = document.querySelector(".left");
+const RightArrow = document.querySelector(".rigth");
+
+RightArrow.addEventListener("click",()=>{
+    currentIndex++;
+ if ( currentIndex >=imagens.length){
+    currentIndex = 0
+ }  
+imgElement.src = imagens[currentIndex];
+});
+LeftArrow.addEventListener("click",()=>{
+    currentIndex--;
+
+if(currentIndex< 0){
+    currentIndex = imagens.length -1;
+
+}
+imgElement.src = imagens[currentIndex]
+});
